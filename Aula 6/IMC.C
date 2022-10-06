27ca79bcbf12235a3dd0d43b1989946d70aec2d8
Inácio <67322986+Inaciocb@users.noreply.github.com>
2022-10-06 15:41:23 -0300

    Update IMC.C

--- Aula 6/IMC.C
+++ Aula 6/IMC.C
@@ -14,14 +14,14 @@ int main()
         	printf("Seu IMC é %.2f e você está abaixo do peso ideal.\n", imc);
 	}else if(imc>=18.6 && imc<=24.9){
 		printf("Seu IMC é %.2f e você está no peso ideal.\n", imc);
-	 }else if(imc>=25 && imc<=29.9){
+	}else if(imc>=25 && imc<=29.9){
 	  	printf("Seu IMC é %.2f e você está levemente acima do peso.\n", imc);
-	  }else if(imc>=30.0 && imc<=34.9){
+	}else if(imc>=30.0 && imc<=34.9){
                	printf("Seu IMC é %.2f e você está com obesidade grau I\n", imc);
-	   }else if(imc>=35 && imc<=39.9){
+	}else if(imc>=35 && imc<=39.9){
 	    	printf("Seu IMC é %.2f e você está com obesidade grau II (severa)\n", imc);
-	    }else if(imc>=40){
+	}else if(imc>=40){
 	     	printf("Seu IMC é %.2f e você está com obesidade grau III (mórbida)\n", imc);
-	     }
+	              }
 	return 0;
 }

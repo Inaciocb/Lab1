dc48559c7b142a45dff13a28376762af33dcdd10
Inácio <67322986+Inaciocb@users.noreply.github.com>
2022-10-08 18:50:40 -0300

    Create Exercicio 4.c

---
+++ Aula 6/Exercicio 4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    float num1, num2, num3;
+    
+    printf("Escreva 3 números: \n Número 1: ");
+    scanf("%f", &num1);
+    printf("Número 2: ");
+    scanf("%f", &num2);
+    printf("Número 3: ");
+    scanf("%f", &num3);
+    
+    if(num1>num2 && num1>num3)
+    {
+        printf("%.2f é o maior", num1);
+    }else if(num2>num1 && num2>num3)
+    {
+        printf("%.2f é o maior", num2);
+    }else if(num3>num1 && num3>num2)
+    {
+        printf("%.2f é o maior", num3);
+    }else{printf("erro");} 
+
+    
+    return 0;
+}

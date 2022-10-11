5fd9722c0349372ed610327760509ebb9ad5bd69
Inácio <67322986+Inaciocb@users.noreply.github.com>
2022-10-11 15:38:22 -0300

    Update Exercicio 1.c

--- Aula 8/Exercicio 1.c
+++ Aula 8/Exercicio 1.c
@@ -5,17 +5,20 @@ int main()
     int x, i=1, num=14;
 
 do{
-    printf("Digite seu chute:");
+    printf("Digite seu chute de 0 a 20: ");
     scanf("%d", &x);
     
-    if(num==x)
+    if(num==x && x>=0 && x<=20)
     {
         printf("Você acertou em %d tentativas, parabéns!", i);
-    }else if(num!=x)
+    }else if(num!=x && x<=20 && x>=0)
     {
         printf("Você errou! Tente novamente\n");
         i++;
+    }else
+    {
+        printf("É entre 0 e 20 seu burro.\n");
     }
-} while(x!=num);
+}while(x!=num);
     return 0;
 }

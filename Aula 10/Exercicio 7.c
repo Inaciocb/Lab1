55c0151ce69b402f3783acccc40fdfeb6bc8f250
Inácio <67322986+Inaciocb@users.noreply.github.com>
2022-10-23 02:34:09 -0300

    Add files via upload

---
+++ Aula 10/Exercicio 7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int antepen(int x)
+{
+   int antepen = (x/100) % 10;
+   return antepen;
+}
+
+int main()
+{
+    int x;
+    printf("digite um valor: ");
+    scanf("%d", &x); //exemplo: 2362
+
+    if(x>=100)
+    {
+        printf("O antepenúltimo é %d\n",antepen(x));
+    }else
+    {
+        printf("Não tem antepenúltimo.\n");
+    }
+    return 0;
+}

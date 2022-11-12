48dcde8b36c21d2ed89c7e94fd6b7b8e619a3a49
Inácio <67322986+Inaciocb@users.noreply.github.com>
2022-11-11 22:42:33 -0300

    Update Exercicio 1.c

--- Aula 14/Exercicio 1.c
+++ Aula 14/Exercicio 1.c
@@ -2,25 +2,21 @@
 
 int main()
 {
-	
-  int v[10];
-	printf("Digite 10 numeros:\n\n");
-	int i = 0;
-  
-  for(i = 0; i <10 ; i++)
-  {
-		printf("N%d: ", i + 1);
-		scanf("%d", &v[i]);
-	}
-  
-  i--;
-  
-  printf("Os números digitados em ordem invertida são:\n");
+    int v[10];
+    printf("Digite 10 numeros:\n\n");
+    int i = 0;
 
-	while(i >= 0)
-	{
-		printf("%d\n", v[i]);
-		i--;
-	}
+    for(i = 0; i <10 ; i++)
+    {
+        printf("N%d: ", i + 1);
+        scanf("%d", &v[i]);
+    }
+    i--;
+    printf("Os números digitados em ordem invertida são:\n");
 
+    while(i >= 0)
+    {
+        printf("%d\n", v[i]);
+        i--;
+    }
 }

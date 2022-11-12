aee7fcdc5bf8907f2b1c1a9079e6dc524e6429be
Inácio <67322986+Inaciocb@users.noreply.github.com>
2022-11-11 22:40:24 -0300

    Imprimir na ordem invertida em que foram digitados

--- Aula 14/Exercicio 1.c
+++ Aula 14/Exercicio 1.c
@@ -2,5 +2,25 @@
 
 int main()
 {
-return 0;
+	
+  int v[10];
+	printf("Digite 10 numeros:\n\n");
+	int i = 0;
+  
+  for(i = 0; i <10 ; i++)
+  {
+		printf("N%d: ", i + 1);
+		scanf("%d", &v[i]);
+	}
+  
+  i--;
+  
+  printf("Os números digitados em ordem invertida são:\n");
+
+	while(i >= 0)
+	{
+		printf("%d\n", v[i]);
+		i--;
+	}
+
 }

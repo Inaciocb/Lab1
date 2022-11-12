ae36927b7a4e3dea82164276ae093832416afacc
Inácio <67322986+Inaciocb@users.noreply.github.com>
2022-11-11 23:52:50 -0300

    Update Exercicio 2.c

--- Aula 14/Exercicio 2.c
+++ Aula 14/Exercicio 2.c
@@ -12,38 +12,47 @@ int main()
         scanf("%d", &v1[i]);
     }
     i=0;
+    printf("\n\n");
     for(i = 0; i < 5; i++)
     {
-        printf("digite o v1[%d]: ", i);
+        printf("digite o v2[%d]: ", i);
         scanf("%d", &v2[i]);
     }
     
     i=0;
-    int j = 10;
     
+    for(i = 0; i < 5; i++)
+    {
+        v3[i] = v1[i];
+    }
+
+    i = 0;
+    int j = 4;
+    
+    do
+    {
+        v3[i+5] = v2[j];
+        i++, j--;
+    }while(i<5 && j>=0);
+    
+    printf("\nOs vetores são:\n\n");
+    
+    printf("v1:\n\n");
+    for(i = 0; i < 5; i++)
+    {
+                printf("v1[%d] = %d\n",i, v1[i]);
+    }
+    i = 0;
+    printf("\nv2:\n\n");
+    for(i = 0; i<5; i++)
+    {
+               printf("v2[%d] = %d\n",i, v2[i]);
+    }
+    i = 0;  
+    printf("\nv3:\n\n");
     for(i = 0; i < 10; i++)
     {
-        if(i < 5)
-        {
-            v3[i] = v1[i];
-        }else
-        {
-            v3[i] = v2[j];
-            j--;
-        }
+    printf("v3[%d] = %d\n", i, v3[i]);
     }
-    printf("\nOs vetores são:\n\n")
-    for(int j = 0; j < 30; j++)
-    {
-        if(j < 5)
-        {
-            printf("v1:\n\n")
-            for(i = 0; i < 5, i++)
-            {
-                print("v[%d] = %d\n",i, v[i]);
-            }
-        }else if(j <= 10 )
-    }
-   
     return 0;
 }

851605d256d3b7b02a30afeb137a1281a6bc27a8
Inacio <inaciocbuemo@gmail.com>
2022-12-04 20:05:33 -0300

    e2

--- Aula 16/Exercicio 2.c
+++ Aula 16/Exercicio 2.c
@@ -1,20 +1,49 @@
-#include <stdio.h.>
+/*
+Faça uma função que calcula as raízes reais de uma equação do segundo grau. Ela deve retornar as duas raízes através
+de ponteiros, e deve ainda retornar um bool, que informa se as raizes são reais ou complexas(nesse último caso, as
+raízes não são calculadas, e os valores apontados pelos ponteiros são mantidos inalterados).
+*/
 
-float realroots(float a, float b, float c, float *x1, float *x2)
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+bool raizes_reais(double a, double b, double c, double *x1, double *x2)
 {
-    float delta = b*b - 4*a*c;
+    double delta = b*b - 4 * (a*c);
     if(delta < 0)
-        return 0;
-    else
+        return false;
+    else  
     {
         *x1 = (-b + sqrt(delta))/(2*a);
         *x2 = (-b - sqrt(delta))/(2*a);
-        return 1;
-    }
+        return true;
+    } 
 }
 
 int main()
 {
-
+    double a, b, c, x1, x2;
+    
+    printf("Digite as variáveis(a, b e c, respectivamente.): ");
+    
+    printf("\na = ");
+    scanf("%lf", &a);
+    printf("b = ");
+    scanf("%lf", &b);
+    printf("c = ");
+    scanf("%lf", &c);
+    
+    bool teste = raizes_reais(a, b, c, &x1, &x2);
+    
+    switch(teste)
+    {
+        case true:
+            printf("As raízes são reais e são: %lf e %lf\n", x1, x2);
+            break;
+        case false:
+            printf("As raízes são complexas\n");
+            break;
+    }
     return 0;
 }

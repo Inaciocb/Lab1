3d8f57fdd4e9cf25e174ee3945dfbc8c7f41d84b
Inacio <inaciocbuemo@gmail.com>
2022-12-06 15:22:27 -0300

    jogadas possiveis

---
+++ Aula 16/Exercicio 3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int i = 0;
+
+int jogada_valida(int linhaJogada, int colunaJogada, int *colunaJogavel)
+{
+    int linhaJogavel;
+    do
+    {
+        linhaJogavel = rand() % 10;
+        *colunaJogavel = rand() % 10;
+    }while (linhaJogavel == linhaJogada || colunaJogada == *colunaJogavel);
+    return linhaJogavel;
+}
+
+int main()
+{
+    srand(time(NULL));
+    int linhaJogada, colunaJogada, linhaJogavel, colunaJogavel;
+    
+    printf("Jogue uma linha: ");
+    scanf("%d", &linhaJogada);
+    printf("Jogue uma coluna: ");
+    scanf("%d", &colunaJogada);
+    
+    linhaJogavel = jogada_valida(linhaJogada, colunaJogada, &colunaJogavel);
+
+    printf("Jogada possível: linha %d coluna %d\n", linhaJogavel, colunaJogavel);
+    
+    return 0;
+}
